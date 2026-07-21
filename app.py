8352bee15546ccee52305d27f1f358e2d25bd17e
lockansalla <lockansalla@outlook.com>
2026-07-21 21:26:50 +0100

    Update appy

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("🎓 Application Streamlit - Rattrapage Git/GitHub")
+st.title("🎓 Application Streamlit - Rattrapage Git/GitHub ANSALLA")
 st.write("Déploiement réussi via GitHub Actions CD vers Hugging Face Spaces !")
